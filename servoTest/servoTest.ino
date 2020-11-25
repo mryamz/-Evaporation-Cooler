70e352c77539eb1d26f7ee10c0e71bfdaff47075
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2020-11-25 00:36:30 -0800

    couldnt make any progress, updated file

--- servoTest/servoTest.ino
+++ servoTest/servoTest.ino
@@ -1,34 +1,39 @@
 #include <Servo.h>
 
 //We need to install the servo library
-unsigned volatile char* portB = (unsigned char*) 0x25;
-unsigned volatile char* ddrB =  (unsigned char*) 0x24;
-unsigned volatile char* pinB = (unsigned char*) 0x23;
-unsigned volatile char* sreg = (unsigned char*) 0x5F;
-unsigned volatile char* eicrb = (unsigned char*) 0x6A;
-unsigned volatile char* eimsk = (unsigned char*) 0x3D;
-int position=0;
+//unsigned volatile char* portB = (unsigned char*) 0x25;
+//unsigned volatile char* ddrB =  (unsigned char*) 0x24;
+//unsigned volatile char* pinB = (unsigned char*) 0x23;
+//unsigned volatile char* sreg = (unsigned char*) 0x5F;
+//unsigned volatile char* eicrb = (unsigned char*) 0x6A;
+//unsigned volatile char* eimsk = (unsigned char*) 0x3D;
+volatile int position = 80;
+volatile Servo x;
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  9;  
 
-Servo x;
+
 void setup() {
-  *sreg = 0b10000000;
-  pinMode(*ddrB, OUTPUT);
-  pinMode(2,INPUT);
+  Serial.begin(9600);
+  //*sreg = 0b10000000;
+  pinMode(ledPin,OUTPUT);
+  pinMode(buttonPin,INPUT);
+  attachInterrupt(digitalPinToInterrupt(0),onButtonPress,FALLING);
   x.attach(9);
   x.write(position);
-  *eicrb = 0b00000011;
-  *eimsk = 0x10;
+  //*eicrb = 0b00000011;
+  //*eimsk = 0x10;
 }
 
 void loop() 
 {
-  
+  Serial.println(position);
+  delay(500);
 }
 
-ISR(INT4_vect)
+void onButtonPress()
 {
-  
-  position += 90;
+  position = position + 90;
   if(position >= 180);
   {
     position = 0;
