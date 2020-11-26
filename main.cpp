6315da7c5050ef9a963e7f93ddd8b75055303de3
Michael Dorado <52438439+mdoradocode@users.noreply.github.com>
2020-11-26 15:26:10 -0800

    Implemeted 3 objects
    This implements the LCD screen, temp and humidity sensor and the servo motor

--- main.cpp
+++ main.cpp
@@ -1,69 +1,97 @@
+#include <dht.h>//Temp and humidity sensor lib
+#include <LiquidCrystal.h> //LCD lib
 #include <Arduino.h>
-#include <Servo.h>
+#include <Servo.h>//Servo lib
 
-unsigned volatile char* portB = (unsigned char*) 0x25;
-unsigned volatile char* ddrB =  (unsigned char*) 0x24;
-unsigned volatile char* pinB = (unsigned char*) 0x23;
-unsigned volatile char* sreg = (unsigned char*) 0x5F;
-unsigned volatile char* eicrb = (unsigned char*) 0x6A;
-unsigned volatile char* eimsk = (unsigned char*) 0x3D;
+//Following declerations are needed to run the LCD monitor                  
+LiquidCrystal lcd (4,5,6,7,8,9); // Define LCD display pins RS, E, D4, D5, D6, D7: Note these are digita; pin 4-9
 
-int water_level_value; // not wet when 0, fully wet when 350
-int water_level_pin = A5; // USING analogue PIN 5 hll
+//The following declerations are the pins needed to run the servo and fan
+int servoPin = 13; //The pin going to the servo is digital pin 13
+int servoButtonPin = 0; //The pin for the wire going from the button to the arduino is digital pin 2, not digital pin 0
+int fanPosition = 0;//This value keeps track of what angle the servo is at
+unsigned long servoButtonTime = 0;//This is used to debounce the servo input
+unsigned long servoButtonLastTime = 0;//This is used to debounce the servo input
+Servo motor;//This is the servo motor variable 
 
-int position=0;
-Servo x;
+//The following declerations are needed to run the humidity and Temp sensor
+#define DHT11_PIN 12 //The pin for this sensor is Digital pin 12
+dht DHT;
+float currentTemp;
+float currentHumid;
 
-
-
-
-enum STATES {
-  LOW_WATER
-}
-
-STATES currentState;
-
-void OnLowWaterState() {
-  currentState = STATES.LOW_WATER;
-}
-
-void setup() {
-  *sreg = 0b10000000;
-  pinMode(*ddrB, OUTPUT);
-  pinMode(2,INPUT);
-  x.attach(9);
-  x.write(position);
-  *eicrb = 0b00000011;
-  *eimsk = 0x10;
-}
-
-void updateWaterLevelReading() {
-  water_level_value = analogRead(water_level_pin); /// returns a max value of 350 when full
-  if(water_level_value < 25 && currentState != STATES.LOW_WATER) {
-    OnLowWaterState(); // tap this function once if we need to change states to low water
-  }
+int x = 0;
+void setup() 
+{
+  intializeServo();//Function to intialize all servo parts
+  initalizeLCD();//Function to intialize all the parts needed for the LCD monitor
+  Serial.begin(9600);//Begin Serial communication
 }
 
 void loop() 
 {
-  updateWaterLevelReading();
-  delay(500);
+  int chk = DHT.read11(DHT11_PIN); //This just needs to be at the start of every loop or the temp and humidity sensor doesnt work
+  updateTempAndHumid();//Gets and sets the global variable "currentTemprature" to the current temp
+  updateLCD();//This function new readings from updateTempAndHumid()
 }
 
-ISR(INT4_vect)
+void updateLCD()
 {
-  delay(500);
-  position += 90;
-  delay(500);
-  if(position > 190)
+  
+  lcd.setCursor(0,0);//Begin writing at the first line, first spot
+  lcd.print("Current Temp: ");
+  lcd.print(currentTemp);
+  lcd.setCursor(0,1);//Begin writing at the second line, first spot
+  lcd.print("Current Humid: ");
+  lcd.print(currentHumid);
+}
+
+
+void initalizeLCD()
+{
+  lcd.begin(16,2); //Set the variable lcd to be a 16x2
+  lcd.setCursor(0,0);//Begin writing at the first line, first spot
+  lcd.print("Swamp Cooler");                                   
+  lcd.setCursor(0,1);//Begin writing at the second line, first spot
+  lcd.print("Simulation");
+  delay(3000);                                             
+  lcd.clear();                                              
+}
+//Delays are needed in this function or else we get garbage values for the temp sensor. DHT is assigned to the digital pin 12 on the board
+void updateTempAndHumid()
+{
+  delay(575);
+  currentTemp = DHT.temperature; //update current temp global variable
+  currentHumid = DHT.humidity;//update current humidity global variable
+  delay(575);
+  
+}
+
+
+
+void intializeServo()
+{
+ 
+  pinMode(servoPin, OUTPUT);//Set digital pin 13 to output
+  motor.attach(13);//Attach the motor variable to pin 13
+  motor.write(fanPosition);//Write the intial servo position (0);
+  attachInterrupt(servoButtonPin, servoButton, RISING);//Create the interrupt for digital pin 2(that is zero internally), name the ISR function, and trigger the change on the rising edge only
+}
+
+//This function adds 90 degrees to the servos current position until it reaches 180 degrees and then resets back down to zero, it also uses time stamps and the milli function to debouce the button
+void servoButton()
+{
+  servoButtonTime = millis();
+  if (servoButtonTime - servoButtonLastTime > 250)
   {
-    position = 0;
+    {
+      fanPosition += 90;
+      if(fanPosition > 180)
+        {
+          fanPosition = 0;
+        }
+      motor.write(fanPosition);
+      servoButtonLastTime = servoButtonTime;
+    }
   }
-  delay(10000);
-  x.write(position);
-
-
-
-
-  delay(500);
 }
